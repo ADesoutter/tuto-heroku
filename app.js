07799e03c025cd1ec68748269cb503c01805f77c
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-15 11:41:50 +0200

    Get one post in JSON with express url

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/posts', (req, res) => {
+    res.send({
+        id: 1,
+        title: "Lorem ipsum dolor sit amet",
+        description: "kfokfokfokfofkofko kokjdf isjidjf idj"
+    })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
